6b92ea35b4ef2ad4b5c43a1fc14fded7077bba88
atifibrahim7 <bigdream382@gmail.com>
2024-08-16 20:17:50 +0500

    .

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -24,6 +24,7 @@ const Header = () => {
             height={1500}
             layout="responsive"
           />
+          <img src="/images/block.png" alt="" />
         </div>
       </div>
     </div>
