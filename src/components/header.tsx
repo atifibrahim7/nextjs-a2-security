ba2654d279a9d0d4fa49004912bc1dbd2e8052e8
atifibrahim7 <bigdream382@gmail.com>
2024-08-16 20:12:46 +0500

    block added

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,19 +1,33 @@
 import React from "react";
 import styles from "../styles/header.module.css";
-const Section1 = () => {
+import Image from "next/image";
+const Header = () => {
   return (
     <div>
-      <div className={styles.main}>
-        <h1>Blockchain Security Audit Services</h1>
-      </div>
-      <div className={styles.description}>
-        <p>
-          A2 Security combine high-end security research with a real-world
-          attacker mentality to reduce risk and fortify code.
-        </p>
+      <div className={styles.container}>
+        <div className="wrapper">
+          <div className={styles.main}>
+            <h1>Blockchain Security Audit Services.</h1>
+          </div>
+          <div className={styles.description}>
+            <p>
+              A2 Security combine high-end security research with a real-world
+              attacker mentality to reduce risk and fortify code.
+            </p>
+          </div>
+        </div>
+        <div className={styles.img}>
+          <Image
+            src="/images/block.svg"
+            alt="block"
+            width={1500}
+            height={1500}
+            layout="responsive"
+          />
+        </div>
       </div>
     </div>
   );
 };
 
-export default Section1;
+export default Header;
