a5d711499afd02f79279111a7b9bc4a57402bc66
atifibrahim7 <bigdream382@gmail.com>
2024-08-17 13:31:33 +0500

    Navbar working

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,14 +1,48 @@
 import React from "react";
-import styles from "@/styles/Home.module.css";
+import styles from "@/styles/navbar.module.css";
 
-const Navbar = () => {
+type NavbarProps = {
+  option: "state1" | "state2" | "state3";
+  refs: {
+    headerRef: React.RefObject<HTMLDivElement>;
+    section2Ref: React.RefObject<HTMLDivElement>;
+    section3Ref: React.RefObject<HTMLDivElement>;
+  };
+};
+
+const Navbar = ({ option, refs }: NavbarProps) => {
+  const navbarClass =
+    option === "state1"
+      ? styles.navbar1
+      : option === "state2"
+      ? styles.navbar2
+      : styles.navbar3;
+  const navbarLinksClass =
+    option === "state1"
+      ? styles.navbarlink1
+      : option === "state2"
+      ? styles.navbarlink2
+      : styles.navbarlink3;
+  const scrollToSection = (ref: React.RefObject<HTMLDivElement>) => {
+    if (ref.current) {
+      ref.current.scrollIntoView({ behavior: "smooth" });
+    }
+  };
   return (
     <div>
-      <div className={styles.navbar}>
-        <h2>A2 Security</h2>
-        <div className={styles.navbarlinks}>
+      <div className={navbarClass}>
+        <div>
+          <h2>A2 SECURITY</h2>
+        </div>
+        <div className={navbarLinksClass}>
           <div>
-            <a href="https://nextjs.org/docs">Work</a>
+            <a
+              onClick={() => {
+                scrollToSection(refs.section2Ref);
+              }}
+            >
+              Work
+            </a>
           </div>
           <div>
             <a href="https://nextjs.org/learn">Services</a>
@@ -24,4 +58,5 @@ const Navbar = () => {
     </div>
   );
 };
+
 export default Navbar;
