f7e9f54903ee160826a99e73fc1bc84123bf3e55
Atif <aatiifibrahim@gmail.com>
2024-08-20 21:18:31 +0500

    slide track

--- src/components/SlideTrack.tsx
+++ src/components/SlideTrack.tsx
@@ -1,51 +1,31 @@
 import styles from "@/styles/SlideTrack.module.css";
 import React from "react";
 import Image from "next/image";
+
 const Slider = () => {
   return (
-    <div>
-      <div className={styles.slider}>
-        <div className={styles.slidetrack}>
-          <div className={styles.slide}>
-            <Image
-              src={"/images/google.png"}
-              alt="Logo"
-              width={100}
-              height={100}
-            />
-          </div>
-          <div className={styles.slide}>
-            <Image
-              src={"/images/google.png"}
-              alt="Logo"
-              width={100}
-              height={100}
-            />
-          </div>{" "}
-          <div className={styles.slide}>
-            <Image
-              src={"/images/google.png"}
-              alt="Logo"
-              width={100}
-              height={100}
-            />
-          </div>{" "}
-          <div className={styles.slide}>
-            <Image
-              src={"/images/google.png"}
-              alt="Logo"
-              width={100}
-              height={100}
-            />
-          </div>{" "}
-          <div className={styles.slide}>
-            <Image
-              src={"/images/google.png"}
-              alt="Logo"
-              width={100}
-              height={100}
-            />
-          </div>
+    <div className={styles.slider}>
+      <div className={styles.slidetrack}>
+        <div className={styles.slide}>
+          <Image src="/images/google.png" alt="Logo" width={100} height={100} />
+        </div>
+        <div className={styles.slide}>
+          <Image src="/images/google.png" alt="Logo" width={100} height={100} />
+        </div>
+        <div className={styles.slide}>
+          <Image src="/images/google.png" alt="Logo" width={100} height={100} />
+        </div>
+        <div className={styles.slide}>
+          <Image src="/images/google.png" alt="Logo" width={100} height={100} />
+        </div>
+        <div className={styles.slide}>
+          <Image src="/images/google.png" alt="Logo" width={100} height={100} />
+        </div>
+        <div className={styles.slide}>
+          <Image src="/images/google.png" alt="Logo" width={100} height={100} />
+        </div>
+        <div className={styles.slide}>
+          <Image src="/images/google.png" alt="Logo" width={100} height={100} />
         </div>
       </div>
     </div>
