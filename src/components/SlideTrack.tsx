38d851f8657bd9ef3f351f01186301ab723795db
atifibrahim7 <bigdream382@gmail.com>
2024-08-23 12:36:11 +0500

    slider update ,

--- src/components/SlideTrack.tsx
+++ src/components/SlideTrack.tsx
@@ -2,14 +2,24 @@ import styles from "@/styles/SlideTrack.module.css";
 import React from "react";
 
 const logos = [
-  "./logos/3m.svg",
-  "./logos/barstool-store.svg",
-  "./logos/budweiser.svg",
-  "./logos/buzzfeed.svg",
-  "./logos/forbes.svg",
-  "./logos/macys.svg",
-  "./logos/menshealth.svg",
-  "./logos/mrbeast.svg",
+  "./logos/aave_3.1.png",
+  "./logos/chainlink_ccip.jpg",
+  "./logos/Curvance.jpg",
+  "./logos/dittoEth.png",
+  "./logos/dyad.jpg",
+  "./logos/ebtc.jpg",
+  "./logos/Euler.png",
+
+  "./logos/exactly-protocol.png",
+  "./logos/folksfinance.png",
+  "./logos/pooltogether.jpg",
+  "./logos/sizeCredit.jpg",
+  "./logos/sofa.jpg",
+  "./logos/stakelink.png",
+  "./logos/steadfi.png",
+  "./logos/superform.png",
+  "./logos/wormhole_ntt.jpg",
+  "./logos/zeroland.jpg",
 ];
 
 const Slider: React.FC = () => {
